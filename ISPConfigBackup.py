545c4a4369364cf3df5ecc19b12c63d413b113df
Sergio Brighenti <sergio@brighenti.me>
2016-02-08 11:28:30 +0100

    removed deprecated functions, fixed double copy of subdomains, removing logs before compressing

--- ISPConfigBackup.py
+++ ISPConfigBackup.py
@@ -2,6 +2,7 @@ from time import gmtime, strftime
 import os
 import tempfile
 import shutil
+import subprocess
 
 #######--CONFIG--#######
 DB_USER = 'root'
@@ -14,7 +15,7 @@ temp_folder_databases = '/databases/'
 temp_folder_sites = '/sites/'
 
 print(' * Fetching databases...')
-databases = os.popen('mysql --user=' + DB_USER + ' --password=' + DB_PASSWORD + ' -e "SHOW DATABASES;" | tr -d "| " | grep -v Database').read().split('\n')
+databases = subprocess.check_output('mysql --user=' + DB_USER + ' --password=' + DB_PASSWORD + ' -e "SHOW DATABASES;" | tr -d "| " | grep -v Database', shell=True).split('\n')
 databases = [x for x in databases if x]
 
 print(' * Creating folders...')
@@ -28,14 +29,19 @@ for db in databases:
         os.system('mysqldump --user=' + DB_USER + ' --password=' + DB_PASSWORD + ' ' + db + ' > ' + temp_folder + temp_folder_databases + db + '.sql')
 
 print('-- Backup sites')
-sites = os.popen('ls /var/www/ | grep "\."').read().split('\n')
+sites = subprocess.check_output('ls /var/www/ | grep "\."', shell=True).split('\n')
 sites = [x for x in sites if x]
 for site in sites:
+    if site.count('.') >= 2:
+        continue
     print(' * Saving ' + site + '...')
     os.system('cp -Lr /var/www/' + site + ' ' + temp_folder + temp_folder_sites + site + '/')
     if os.path.isdir(temp_folder + temp_folder_sites + site + '/backup'):
         print(' * Removing old ' + site + ' saved backups...')
-        os.system('rm -rf ' + temp_folder + temp_folder_sites + site + '/backup')
+        shutil.rmtree(temp_folder + temp_folder_sites + site + '/backup')
+    if os.path.isdir(temp_folder + temp_folder_sites + site + '/log'):
+        print(' * Removing ' + site + ' logs...')
+        shutil.rmtree(temp_folder + temp_folder_sites + site + '/log')
 
 print('-- Compressing...')
 os.system('cd ' + temp_folder + ' && tar -zcf ' + BACKUP_DIR + '/ispconfig_' + strftime("%d-%m-%Y_%H:%M:%S", gmtime()) + '.tar.gz *')
